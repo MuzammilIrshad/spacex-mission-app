5f53949fc97bb4787c765a22ae998337ae481873
MuzammilIrshad <muzammilirshad931@gmail.com>
2021-06-02 11:32:01 +0500

    updated files

--- src/components/launchDetails/LaunchDetails.tsx
+++ src/components/launchDetails/LaunchDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { LaunchInfoQuery } from '../../generated/graphql';
 import './Launch.css';
 
@@ -9,14 +9,20 @@ interface Props {
 }
 
 export const LaunchDetails: React.FC<Props> = ({ data }) => {
-
+ 
     return (
         <div>
+            <div className="details">
             <h1>{data.launch?.mission_name}</h1>
             <h3>{data.launch?.rocket?.rocket_name}</h3>
-            <h5>TEST: {data.launch?.launch_success ? "Success" : "Failed"}</h5>
-            <span className="details">{data.launch?.details}</span>
-            <div>
+            <h5 style={data.launch?.launch_success ? { color: "green" } : { color: "red" }}>TEST: {data.launch?.launch_success ? "Success" : "Failed"}</h5>
+            </div>
+            { data.launch?.details ? ( 
+                <div>
+                    <span>{data.launch?.details}</span>
+                </div>
+            ):""}
+                <div>
             {!!data.launch?.links?.flickr_images ? data.launch.links.flickr_images.map((image) => {
                 console.log(image);
                 return <img src={String(image)} className="images"/>
